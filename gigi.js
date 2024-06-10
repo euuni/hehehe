7cb73eaf35325b01083a92527edc2e61e59265dc
eunni <choidy0106@gmail.com>
2024-06-10 22:26:39 +0900

    hh

--- gigi.js
+++ gigi.js
@@ -1,20 +1,26 @@
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const button3 = document.getElementById('button3');
-const button4 = document.getElementById('button4');
+function navigateWithConfirmation(url) {
+  const userConfirmed = confirm("페이지로 이동하겠습니다. 계속하시겠습니까?");
+  if (userConfirmed) {
+      window.location.href = url;
+  }
+}
 
-button1.addEventListener('click', () => {
-  window.location.href = "https://eunnihp.vercel.app/";
+document.getElementById('button1').addEventListener('click', () => {
+  navigateWithConfirmation("https://eunnihp.vercel.app/");
 });
 
-button2.addEventListener('click', () => {
-  window.location.href = "https://nextjs-lemon-six-16.vercel.app/";
+document.getElementById('button2').addEventListener('click', () => {
+  navigateWithConfirmation("https://nextjs-lemon-six-16.vercel.app/");
 });
 
-button3.addEventListener('click', () => {
-  window.location.href = "https://website-three-gamma-68.vercel.app/";
+document.getElementById('button3').addEventListener('click', () => {
+  navigateWithConfirmation("https://website-three-gamma-68.vercel.app/");
 });
 
-button4.addEventListener('click', () => {
-  window.location.href = "https://blog2-beta-tawny.vercel.app/";
+document.getElementById('button4').addEventListener('click', () => {
+  navigateWithConfirmation("https://blog2-beta-tawny.vercel.app/");
+});
+
+document.getElementById('button5').addEventListener('click', () => {
+  navigateWithConfirmation("https://teamproject-sigma.vercel.app/");
 });
